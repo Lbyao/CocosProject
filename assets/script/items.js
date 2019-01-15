0f171587eb4cac755869b0e7db07e29023cb690f
lmr <1483587277@qq.com>
2019-01-15 10:23:03 +0800

    修改列表

--- assets/script/items.js
+++ assets/script/items.js
@@ -8,11 +8,20 @@
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
 
+import global from "./global"
 cc.Class({
     extends: cc.Component,
 
     properties: {
         label: {
+            default: null,
+            type: cc.Label
+        },
+        loadingNode: {
+            default: null,
+            type: cc.Node
+        },
+        loadLabel: {
             default: null,
             type: cc.Label
         },
@@ -28,37 +37,78 @@ cc.Class({
         //     var label = this.node.getComponent(cc.Label);
         //     cc.log(label);
         // }, this)
+        var that = this;
         this.node.on('touchend', function () {
-            console.log("Item " + this.itemID + ' clicked');
+            console.log("Item " + that.itemID + ' clicked');
             // var label = this.node.getComponent(cc.Label);
-            cc.log(this.label.string);
-            Global.MusicName = this.label.string;
-            this.downLoad();
-            cc.director.loadScene("game", this.onSceneLaunched());
+            cc.log(that.label.string);
+            Global.MusicName = that.label.string;
+            // this.downLoad();
+            // cc.log(cc.sys.localStorage.getItem("00")+"-----------getItem")
+            var itemsid = that.getDownloadId(that.itemID);
+            cc.log(cc.sys.localStorage.getItem(itemsid)+"-----------getItem")
+            if(cc.sys.localStorage.getItem(itemsid)==null){
+                var fileName = itemsid+".zip";
+                var url = "http://www.dadpat.com/app/rhythm/"+fileName;
+                that.downFile2Local(url,fileName,function(params) {
+                    cc.log(params);
+                });
+            }
             
+            // cc.director.loadScene("game", this.onSceneLaunched());
+                
         }, this);
-        this.label.string = "123"
+        // this.label.string = "123"
     },
 
+    getDownloadId(itemName) {
+        var id;
+        switch(itemName){
+            case "三只小熊":
+                id = '00'
+                break;
+            case "踏雪寻梅":
+                id = '01'
+                break;
+            case "小白船":
+                id = '02'
+                break;
+            case "你好":
+                id = '03'
+                break;
+            default:
+                id = '10'
+                break;
+        }
+        return id;
+    },
+
+    // 短链接
     downLoad (){
-        var xhr = new XMLHttpRequest();
+        var xhr = cc.loader.getXMLHttpRequest();
         xhr.timeout = 3000;
 
         xhr.ontimeout = function (e) {
             // 监听timeout
         }
         
+        xhr.open("GET","https://httpbin.org/get?show_env=1",true);
+        if (cc.sys.isNative) {
+            xhr.setRequestHeader("Accept-Encoding","gzip,deflate");
+        }
     
-        xhr.open("GET","http://www.wanandroid.com/article/list/0/json",true);
-        xhr.withCredentials = true;
-        // xhr.setRequestHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        xhr.setRequestHeader("Access-Control-Allow-Origin","*");
-        // xhr.setRequestHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me")
-        // xhr.setRequestHeader("Origin","*");
-        
-    
-        cc.log(xhr.getResponseHeader("Access-Control-Allow-Origin"));
         xhr.send();
+
+        ['loadstart', 'abort', 'error', 'load', 'loadend', 'timeout'].forEach(function (eventname) {
+            xhr["on" + eventname] = function () {
+                cc.log("\nEvent : " + eventname);
+                // eventLabel.string = eventLabelOrigin + "\nEvent : " + eventname;
+                if (eventname === 'timeout') {
+                    cc.log('(timeout)');
+                    // label.string = '(timeout)';
+                }
+            };
+        });
 
         xhr.onreadystatechange = function(){
             if(xhr.readyState==4&&((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304)){
@@ -66,6 +116,39 @@ cc.Class({
                 console.log(request)
             }
         };
+    },
+
+    downFile2Local:function(url, fileName, callback){
+        // data/data/org.cocos2d.TestProject/file/fileName
+        var fullPath = jsb.fileUtils.getWritablePath() + fileName
+        var that = this;
+        // showModelLoadingDialog();//显示加载中模态框
+        this.loadingNode.opacity = 255;
+        this.loadLabel.string = "开始！";
+        cc.log("fullPath:"+fullPath);
+        this.downloader = new jsb.Downloader();
+        this.downloader.createDownloadFileTask(url, fullPath,"");//创建下载任务
+        this.downloader.setOnFileTaskSuccess(function(){   
+            cc.log("下载成功!!");      
+            // removeModelLoadingDialog();//删除加载中模态框
+            cc.sys.localStorage.setItem(that.getDownloadId(that.itemID),fullPath);
+            if(callback){
+                callback(fullPath);
+            }
+        });
+
+        this.downloader.setOnTaskProgress(function(task, bytesReceived, totalBytesReceived, totalBytesExpected) {
+            cc.log("已下载完成的大小："+bytesReceived);
+            cc.log("总大小:"+totalBytesReceived);
+            cc.log("预期总大小:"+totalBytesExpected);
+        });
+
+        this.downloader.setOnTaskError(function(){  
+            cc.log("下载失败!!");   
+            // removeModelLoadingDialog();
+            // showTips("文件下载失败！！！");
+        });
+        
     },
 
     onSceneLaunched: function(){
